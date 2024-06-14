26aa096fa9f9204f7fa7878972f451eff07a5c1d
shuaibasghar <shuaibasghargtk@gmail.com>
2024-06-14 16:41:51 +0500

    product and shop component design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import Header from "./components/Header";
+import Shop from "./components/Shop";
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState({ items: [] });
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart)=>{
+      const updatedItems
+    })
+
+  }
   return (
     <>
       <Header />
+      <Shop onAddItemToCart={handleAddItemToCart} />
     </>
   );
 }
